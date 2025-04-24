4b981556a2c966478c7502c764146e7e32166f39
unknown <ashish11to12@gmail.com>
2025-04-24 17:46:23 +0530

    started bulding user and admin service for code Maze

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+import { PORT } from './config/server.config';
+const app=express();
+app.use(express.json());
+
+app.listen(PORT,()=>{console.log(`Server is running on port ${PORT}`)});
